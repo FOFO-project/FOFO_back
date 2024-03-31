f8b8885d94cb0cc4e8a7739f6010e051f63fcdc0
양현승 <happyman1104@naver.com>
2024-03-31 11:30:03 +0900

    #7-엔티티 추가 개선

--- src/main/java/com/fofo/core/member/entity/HashTag.java
+++ src/main/java/com/fofo/core/member/entity/HashTag.java
@@ -1,15 +1,21 @@
 package com.fofo.core.member.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.ConstraintMode;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
-import lombok.Setter;
-
-import java.sql.Timestamp;
-import java.time.LocalDateTime;
 
 @Entity
-@Getter @Setter
-public class HashTag {
+@Getter
+public class HashTag extends BaseEntity{
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "hashtag_id")
     private Long id;
@@ -18,9 +24,7 @@ public class HashTag {
     @JoinColumn(name = "member_id", nullable = true, foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
     private Member member;
 
+    @Size(max=10)
     private String content;
 
-    private LocalDateTime createdTime;
-
-    private LocalDateTime updatedTime;
 }
