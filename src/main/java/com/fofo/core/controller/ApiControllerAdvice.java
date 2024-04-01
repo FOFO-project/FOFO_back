5ff4a74156b6ab73fb4b169b2cbdf5c020ea8ed3
sungjun1116 <opr99@naver.com>
2024-04-01 23:37:46 +0900

    #12-@RestControllerAdvice 개발

--- src/main/java/com/fofo/core/controller/ApiControllerAdvice.java
+++ src/main/java/com/fofo/core/controller/ApiControllerAdvice.java
@@ -1,4 +1,31 @@
 package com.fofo.core.controller;
 
+import com.fofo.core.support.error.CoreApiException;
+import com.fofo.core.support.error.CoreErrorType;
+import com.fofo.core.support.response.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
 public class ApiControllerAdvice {
+
+    @ExceptionHandler(CoreApiException.class)
+    public ResponseEntity<ApiResponse<?>> handleCoreApiException(CoreApiException e) {
+        switch (e.getErrorType().getLogLevel()) {
+            case ERROR -> log.error("CoreApiException : {}", e.getMessage(), e);
+            case WARN -> log.warn("CoreApiException : {}", e.getMessage(), e);
+            default -> log.info("CoreApiException : {}", e.getMessage(), e);
+        }
+        return new ResponseEntity<>(ApiResponse.error(e.getErrorType(), e.getData()), e.getErrorType().getStatus());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse<?>> handleException(Exception e) {
+        log.error("Exception : {}", e.getMessage(), e);
+        return new ResponseEntity<>(ApiResponse.error(CoreErrorType.DEFAULT_ERROR), CoreErrorType.DEFAULT_ERROR.getStatus());
+    }
+    
 }
