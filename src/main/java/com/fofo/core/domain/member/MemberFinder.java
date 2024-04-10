25576908b486005f6855e88ac080d3edb2454586
양현승 <happyman1104@naver.com>
2024-04-10 15:19:27 +0900

    #13-원복

--- src/main/java/com/fofo/core/domain/member/MemberFinder.java
+++ src/main/java/com/fofo/core/domain/member/MemberFinder.java
@@ -1,17 +1,7 @@
 package com.fofo.core.domain.member;
 
-import com.fofo.core.storage.MemberEntity;
-import com.fofo.core.storage.MemberRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class MemberFinder {
-
-    final private MemberRepository memberRepository;
-
-    public MemberEntity findMember(Long memberId) {
-        return memberRepository.findById(memberId).orElseThrow();
-    }
 }
