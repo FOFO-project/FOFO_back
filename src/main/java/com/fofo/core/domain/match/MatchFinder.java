a81a33db6ef694d39abd6fc35011a2c803327ffe
양현승 <happyman1104@naver.com>
2024-04-10 15:18:14 +0900

    #13-Exception 케이스 추가

--- src/main/java/com/fofo/core/domain/match/MatchFinder.java
+++ src/main/java/com/fofo/core/domain/match/MatchFinder.java
@@ -1,8 +1,11 @@
 package com.fofo.core.domain.match;
 
-import com.fofo.core.controller.response.MatchResponseDto.MatchResponseDto;
 import com.fofo.core.domain.member.Member;
 import com.fofo.core.storage.MatchRepository;
+import com.fofo.core.storage.MemberEntity;
+import com.fofo.core.storage.MemberRepository;
+import com.fofo.core.support.error.CoreApiException;
+import com.fofo.core.support.error.CoreErrorType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -15,6 +18,7 @@ import java.util.List;
 public class MatchFinder {
 
     private final MatchRepository matchRepository;
+    private final MemberRepository memberRepository;
 
     public List<Member> findMatchPossibleMembers() {
         return null;
@@ -23,5 +27,12 @@ public class MatchFinder {
     public Page<Match> findMatches(final int page, final int size) {
         PageRequest pageRequest = PageRequest.of(page, size);
         return matchRepository.selectMatchResultList(pageRequest);
+    }
+
+    public MemberEntity findMember(Long memberId) {
+        return memberRepository.findById(memberId)
+                .orElseThrow(() -> new CoreApiException(
+                        CoreErrorType.MEMBER_NOT_FOUND_ERROR)
+                );
     }
 }
--- src/main/java/com/fofo/core/domain/match/MatchRemover.java
+++ src/main/java/com/fofo/core/domain/match/MatchRemover.java
@@ -1,6 +1,8 @@
 package com.fofo.core.domain.match;
 
 import com.fofo.core.storage.MatchRepository;
+import com.fofo.core.support.error.CoreApiException;
+import com.fofo.core.support.error.CoreErrorType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -13,6 +15,9 @@ public class MatchRemover {
     private final MatchRepository matchRepository;
 
     public void removeMatch(final List<Long> matchIdList) {
+        if(!matchRepository.findUnCancelableMatches(matchIdList).isEmpty()){
+            throw new CoreApiException(CoreErrorType.MATCH_UNCANCELABLE_ERROR);
+        };
         matchRepository.deleteMatchesBy(matchIdList);
     }
 }
