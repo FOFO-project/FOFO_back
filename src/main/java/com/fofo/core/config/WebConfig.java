b474884598416a13b2e5fcd8d1dc8095d2c6f338
sungjun1116 <opr99@naver.com>
2024-04-12 00:00:30 +0900

    #19-프론트 서버 origin 에 대해 cors 이슈 예외처리 적용

---
+++ src/main/java/com/fofo/core/config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.fofo.core.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration(proxyBeanMethods = false)
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://144.24.85.213:5173")
+                .allowedMethods("*");
+    }
+}
