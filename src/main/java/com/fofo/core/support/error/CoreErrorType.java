d2a976e73f6f7562bc3a93bcb785bbe8cfca7dd3
양현승 <happyman1104@naver.com>
2024-04-18 22:42:40 +0900

    #32 - 에러코드 변경

--- src/main/java/com/fofo/core/support/error/CoreErrorType.java
+++ src/main/java/com/fofo/core/support/error/CoreErrorType.java
@@ -28,7 +28,7 @@ public enum CoreErrorType {
     //Match 관련 에러
     MATCH_ALREADY_COMPLETED_ERROR(HttpStatus.BAD_REQUEST, CoreErrorCode.MATE500, "Match is already COMPLETED.", LogLevel.ERROR),
     MATCH_UNCANCELABLE_ERROR(HttpStatus.BAD_REQUEST, CoreErrorCode.MATE501, "UnCancelable Match exists", LogLevel.ERROR),
-    MATCHABLE_MEMBER_NOT_FOUND(HttpStatus.INTERNAL_SERVER_ERROR, CoreErrorCode.MATE502, "Matchable member is not found", LogLevel.ERROR),
+    MATCHABLE_MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, CoreErrorCode.MATE502, "Matchable member is not found", LogLevel.ERROR),
     MATCH_NOT_FOUND_ERROR(HttpStatus.NOT_FOUND, CoreErrorCode.MATE503, "Match Not found", LogLevel.ERROR);
 
     private final HttpStatus status;
